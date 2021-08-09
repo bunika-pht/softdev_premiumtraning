cb1c02186161d65f6bbc76637cb8a94e35aa94ba
Meryht <yardpiroon.2017@mail.kmutt.ac.th>
2021-08-09 15:10:29 +0700

    Add : normal function
    Create normal function and Pipenv

--- ML models/model.py
+++ ML models/model.py
@@ -1,1 +1,19 @@
-print("hello")
+
+
+from sklearn import linear_model
+
+def model_selection():
+    pass
+
+def dataset_sprit():
+    pass
+
+def taining_model(): 
+    pass
+
+def model_prediction(): #reture 1 or 0
+    pass
+
+def confustion_matrix(): #reture Yes NO
+    pass
+
