e88c5a1d9c2a361b9b30f67b952f1ce6f4b8063b
BUNIKA PHETWITHOOL <bunika.abc@mail.kmutt.ac.th>
2021-11-21 00:34:48 +0700

    delete while true and move setting window to main.py

--- Data_Preprocessing/prep.py
+++ Data_Preprocessing/prep.py
@@ -174,13 +174,6 @@ class DataPreprocess():
     ]
 
 
-main = Main()
-pp = DataPreprocess()
-window_main = sg.Window("AI.EXE", main.Layout, resizable=True, finalize=True)
-window_main.maximize()
-window_main.TKroot.minsize(550, 200)
-window_insert_active = False
-
 # if __name__ == main:
 #     try:
 #         pass
