754b3f4f1b78e0bfc339b8a16a30f10cf47ec855
BUNIKA PHETWITHOOL <bunika.abc@mail.kmutt.ac.th>
2021-12-31 13:43:28 +0700

    last update

--- Data_Preprocessing/main.py
+++ Data_Preprocessing/main.py
@@ -1,5 +1,5 @@
-
 import io
+from os import path
 import PIL
 from PySimpleGUI.PySimpleGUI import Image
 from numpy.core.fromnumeric import size
@@ -8,45 +8,173 @@ import PySimpleGUI as sg
 import shutil
 from PIL import *
 from PIL import Image
-
 import base64
 main = Main()
 pp = DataPreprocess()
 window_main = sg.Window("AI.EXE", pp.Layout_Data_Preprocess,
                         resizable=True, finalize=True)
+# window_main = sg.Window("AI.EXE", pp.Layout_InserDataset,
+#                         resizable=True, finalize=True)
 window_main.maximize()
 window_main.TKroot.minsize(550, 200)
 window_insert_active = False
 
+
+def controller(img, brightness=255,
+               contrast=127):
+
+    brightness = int((brightness - 0) * (255 - (-255)) / (510 - 0) + (-255))
+
+    contrast = int((contrast - 0) * (127 - (-127)) / (254 - 0) + (-127))
+
+    if brightness != 0:
+
+        if brightness > 0:
+
+            shadow = brightness
+
+            max = 255
+
+        else:
+
+            shadow = 0
+            max = 255 + brightness
+
+        al_pha = (max - shadow) / 255
+        ga_mma = shadow
+
+        # The function addWeighted calculates
+        # the weighted sum of two arrays
+        cal = cv.addWeighted(img, al_pha,
+                             img, 0, ga_mma)
+
+    else:
+        cal = img
+
+    if contrast != 0:
+        Alpha = float(131 * (contrast + 127)) / (127 * (131 - contrast))
+        Gamma = 127 * (1 - Alpha)
+
+        # The function addWeighted calculates
+        # the weighted sum of two arrays
+        cal = cv.addWeighted(cal, Alpha,
+                             cal, 0, Gamma)
+
+    # putText renders the specified text string in the image.
+    # cv.putText(cal, 'B:{},C:{}'.format(brightness,
+    #                                     contrast), (10, 30),
+    #             cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+    return cal
+
+
 if __name__ == "__main__":
     class_list = []
     while True:
+        class_combo = os.listdir(r"Class")
         try:
             event, value = window_main.read()
+            window_main["-CLASS COMBO-"].update(values=class_combo)
             if event == WIN_CLOSED:
                 break
-
+            elif event == '-CLASS COMBO-':
+                selected_class = value["-CLASS COMBO-"]
+                window_main["-CLASS NAME-"].Update(selected_class)
+                path_image = os.path.join(r"Class/", selected_class+"/")
+                img_list = os.listdir(path_image)
+                window_main["-FILE LIST-"].update(img_list)
+            elif event == '-FILE LIST-':
+                try:
+                    filename = value["-FILE LIST-"][0]
+                    name, ext = os.path.splitext(filename)
+                    img = cv.imread(os.path.join(path_image, filename))
+                    window_main['-IN HIGH-'].update(img.shape[0])
+                    window_main['-IN WIDTH-'].update(img.shape[1])
+                    data = Image.fromarray(img)
+                    data.thumbnail((600, 300))
+                    bio = io.BytesIO()
+                    b, g, r = data.split()
+                    data = Image.merge("RGB", (r, g, b))
+                    data.save(bio, format="PNG")
+                    window_main['-IMAGE-'].update(data=bio.getvalue(),
+                                                  size=(600, 300))
+                except Exception as e:
+                    print("error:"+str(e))
+            elif event == '-CL-':
+                img = cv.imread(os.path.join(path_image, filename))
+                window_main['-IN HIGH-'].update(img.shape[0])
+                window_main['-IN WIDTH-'].update(img.shape[1])
+                data = Image.fromarray(img)
+                data.thumbnail((600, 300))
+                bio = io.BytesIO()
+                b, g, r = data.split()
+                data = Image.merge("RGB", (r, g, b))
+                data.save(bio, format="PNG")
+                window_main['-IMAGE-'].update(
+                    data=bio.getvalue(), size=(600, 300))
             elif event == '-RZ-':
                 try:
-                    img = cv.imread(r"Class\test113\3.png")
                     dim = (int(value['-IN WIDTH-']), int(value['-IN HIGH-']))
                     resized = cv.resize(img, dim, interpolation=cv.INTER_AREA)
-                    print(type(resized))
                     data = Image.fromarray(resized)
+                    data.thumbnail((600, 300))
                     bio = io.BytesIO()
                     b, g, r = data.split()
                     data = Image.merge("RGB", (r, g, b))
                     data.save(bio, format="PNG")
                     window_main['-IMAGE-'].update(
-                        data=bio.getvalue(), size=dim)
+                        data=bio.getvalue(), size=(600, 300))
+                except Exception as e:
+                    print(str(e))
+
+            elif event == '-TL-':
+                img = cv.rotate(
+                    img, cv.ROTATE_90_COUNTERCLOCKWISE)
+                data = Image.fromarray(img)
+                data.thumbnail((600, 300))
+                bio = io.BytesIO()
+                b, g, r = data.split()
+                data = Image.merge("RGB", (r, g, b))
+                data.save(bio, format="PNG")
+                window_main['-IMAGE-'].update(
+                    data=bio.getvalue(), size=(600, 300))
+            elif event == '-TR-':
+                img = cv.rotate(
+                    img, cv.ROTATE_90_CLOCKWISE)
+                data = Image.fromarray(img)
+                data.thumbnail((600, 300))
+                bio = io.BytesIO()
+                b, g, r = data.split()
+                data = Image.merge("RGB", (r, g, b))
+                data.save(bio, format="PNG")
+                window_main['-IMAGE-'].update(
+                    data=bio.getvalue(), size=(600, 300))
+            elif event == '-SLIDER BRIGHT-':
+                try:
+                    # convert it to hsv
+                    defualt_b = 0
+                    slide_value_b = int(value["-SLIDER BRIGHT-"])
+                    slide_value_c = int(value["-SLIDER CONTRAST-"])
+
+                    img = controller(img, slide_value_b,
+                                     slide_value_c)
+                    data = Image.fromarray(img)
+                    data.thumbnail((600, 300))
+                    bio = io.BytesIO()
+                    b, g, r = data.split()
+                    data = Image.merge("RGB", (r, g, b))
+                    data.save(bio, format="PNG")
+                    window_main['-IMAGE-'].update(
+                        data=bio.getvalue(), size=(600, 300))
+
                 except Exception as e:
                     print(str(e))
             # elif event == '-ADD CLASS-':
             #     folder = value['-FOLDER-']
-
             #     try:
             #         file_list = os.listdir(folder)
-            #     except:
+            #     except Exception as e:
+            #         print(str(e))
             #         file_list = []
             #     fnames = [
             #         f
@@ -55,26 +183,29 @@ if __name__ == "__main__":
             #         and f.lower().endswith((".png", ".jpg", ".jpeg"))
             #     ]
             #     F_classname = value["-CLASS TYPE-"]
-            #     isFolder = os.path.isfile(os.path.join("Class", F_classname))
+            #     Class_path = os.path.join(r"Class", F_classname)
+            #     isFolder = os.path.exists(Class_path)
+
             #     if isFolder == False:
             #         try:
-            #             os.makedirs(os.path.join("Class", F_classname))
-            #             path = os.path.join("Class", F_classname)
+            #             os.makedirs(Class_path)
             #             for i in fnames:
-            #                 shutil.copyfile(os.path.join(folder, i), os.path.join(
-            #                     path, i))
+            #                 shutil.copy2(os.path.join(folder, i), os.path.join(
+            #                     Class_path, i))
             #         except Exception as e:
             #             print(str(e))
-            #     if isFolder == True:
+            #         class_list.append(F_classname)
+            #     elif isFolder == True:
             #         try:
-            #             pass
+            #             for i in fnames:
+            #                 shutil.copy2(os.path.join(folder, i), os.path.join(
+            #                     Class_path, i))
             #         except Exception as e:
             #             print(str(e))
-            #     class_list.append(F_classname)
-            #     print(class_list)
             #     window_main['-CLASS LIST-'].update(class_list)
             # elif event == "-CLASS LIST-":  # A file was chosen from the listbox
             #     try:
+
             #         file_list = os.listdir(os.path.join(
             #             "Class", value["-CLASS LIST-"][0]))
             #         fnames = [
@@ -82,9 +213,39 @@ if __name__ == "__main__":
             #             for f in file_list
             #         ]
             #         window_main['-FILE LIST-'].update(fnames)
-
             #     except Exception as e:
             #         print(str(e))
+            # elif event == "-FILE LIST-":
+            #     try:
+            #         filename = value["-FILE LIST-"][0]
+            #         name, ext = os.path.splitext(filename)
+            #         img = cv.imread(os.path.join(
+            #             os.path.join(Class_path), filename))
+
+            #         data = Image.fromarray(img)
+            #         data.thumbnail((600, 300))
+            #         bio = io.BytesIO()
+            #         b, g, r = data.split()
+            #         data = Image.merge("RGB", (r, g, b))
+            #         data.save(bio, format="PNG")
+            #         window_main['-IMAGE-'].update(data=bio.getvalue(),
+            #                                       size=(600, 300))
+            #     except Exception as e:
+            #         print("error:"+str(e))
+            # elif event == "-DEL IMG-":
+            #     try:
+            #         path_del = os.path.join(
+            #             "Class", value["-CLASS LIST-"][0])
+            #         os.remove(os.path.join(path_del, filename))
+            #     except:
+            #         pass
+            #     file_list = os.listdir(path_del)
+            #     fnames = [
+            #         f
+            #         for f in file_list
+            #         if os.path.isfile(os.path.join(folder, f))
+            #         and f.lower().endswith((".png", ".jpg", ".jpeg"))]
+            #     window_main["-FILE LIST-"].update(fnames)
         except Exception as e:
             print(str(e))
             break
--- Data_Preprocessing/prep.py
+++ Data_Preprocessing/prep.py
@@ -84,7 +84,8 @@ class DataPreprocess():
     image_viewer_colum = [
         [sg.Text("choose an image from the list on the left")],
         [sg.Text(size=(70, 1), key="-IMG NAME-")],
-        [sg.Image(key="-IMAGE1-", size=(70, 500))],
+        [sg.Frame("", layout=[[sg.Image(key="-IMAGE-", size=(600, 300))]])],
+        # [sg.Image(key="-IMAGE1-", size=(70, 500))],
 
         [sg.Text("1/299", key="-N IMG-", size=(70, 1), justification="right")],
 
@@ -106,14 +107,14 @@ class DataPreprocess():
     bright_and_contrast_section = [
         [sg.Text('  brightness'), sg.Text('', key='-TEXT BRIGHT-')],
         [sg.T(' 0', size=(4, 1), key='-MIN BRIGHT-'),
-         sg.Slider((0, 100), key='-SLIDER BRIGHT-', orientation='h',
+         sg.Slider((0, 255*2), default_value=0, key='-SLIDER BRIGHT-', orientation='h',
                    enable_events=True, disable_number_display=True),
-         sg.T(' 100', size=(4, 1), key='-MAX BRIGHT-')],
+         sg.T(' 255', size=(4, 1), key='-MAX BRIGHT-')],
         [sg.Text('  contrast'), sg.Text('', key='-TEXT CONTRAST')],
         [sg.T(' 0', size=(4, 1), key='-MIN CONTRAST-'),
-         sg.Slider((0, 100), key='-SLIDER CONTRAST-', orientation='h',
+         sg.Slider((0, 127*2), default_value=0, key='-SLIDER CONTRAST-', orientation='h',
                    enable_events=True, disable_number_display=True),
-         sg.T(' 100', size=(4, 1), key='-MAX CONTRAST-')]
+         sg.T(' 127', size=(4, 1), key='-MAX CONTRAST-')]
     ]
 
     # '''Blur () [ Gaussian, Median, Blur ]'''
@@ -164,38 +165,42 @@ class DataPreprocess():
     layout_tab = [
         [sg.TabGroup([[sg.Tab('Pre-process', section_column_tab1),
                      sg.Tab('Feature Extraction', feature_extraction_tab2)]], size=(500, 400))],
-        [sg.Button("Export CSV", key="-CSV-")],
+        [sg.Button("Clear", key="-CL-"), sg.Button("Export CSV", key="-CSV-")],
     ]
 
     image_viewer_colum_pp = [
-        [sg.Image(key="-IMAGE-", size=(100, 300))],
+        [sg.Frame("", layout=[[sg.Image(key="-IMAGE-", size=(600, 300))]])],
+
         [
+
+            sg.Column(layout=([sg.Text("Class:"), sg.Text("", key="-CLASS NAME-", size=(5, 1))],
+                              [sg.Combo(values=[], key="-CLASS COMBO-",
+                                        enable_events=True, size=(10, 0))])),
             sg.Listbox(values=[], enable_events=True,
-                       size=(100, 5), key="-FILE LIST-")
+                       size=(60, 5), key="-FILE LIST-")
         ],
-
-    ]
-    List_viewer_column = [
-
-        [sg.Listbox(values=[], enable_events=True,
-                    size=(10, 5), key="-CLASS LIST-")],
 
     ]
 
     Layout_Data_Preprocess = [
         [sg.Text("Data Preprocessing")],
-        [sg.Column(List_viewer_column), sg.Column(image_viewer_colum_pp, element_justification='left'),
+        [sg.Column(image_viewer_colum_pp, element_justification='left'),
          sg.VSeperator(), sg.Column(layout_tab, element_justification='right')],
         Code_generate.Layout,
 
     ]
 
 
-# if __name__ == main:
-#     try:
-#         pass
-#     except:
-#         pass
+# main = Main()
+# pp = DataPreprocess()
+# # window_main = sg.Window("AI.EXE", pp.Layout_Data_Preprocess,
+# #                         resizable=True, finalize=True)
+# window_main = sg.Window("AI.EXE", main.Layout(),
+#                         resizable=True, finalize=True)
+# window_main.maximize()
+# window_main.TKroot.minsize(550, 200)
+# window_insert_active = False
+
 # while True:
 #     # * menu page
 #     event1, values1 = window_main.read()
@@ -232,7 +237,8 @@ class DataPreprocess():
 #                     filename = os.path.join(
 #                         values2["-FOLDER-"], values2["-FILE LIST-"][0])
 #                     window_insert_data["-TOUT1-"].update(filename)
-#                     window_insert_data["-IMAGE1-"].update(filename=filename)
+#                     window_insert_data["-IMAGE1-"].update(
+#                         filename=filename)
 #                 except:
 #                     pass
 #             elif event2 == "-DEL IMG-":
